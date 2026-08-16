196fa3ee82c6883708cf6297bb27a17ef10c05d8
Nguyen Thai Hoang <hoangthai.personal@gmail.com>
2026-08-16 13:36:35 +0700

    pwa: refresh shell cache for bilingual theme update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rootvalue-v1-20260816';
+const CACHE='rootvalue-v1-20260816b';
 const STATIC=['./','./index.html','./styles.css','./app.js','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
